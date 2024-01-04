52bc52732fe8e2a39fdab9a7824c9b1c091a4b58
Paulo Aboim Pinto <aboimpinto@gmail.com>
2024-01-04 23:17:34 +0100

    Initial commit

---
+++ ServiceCollectionManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Olimpo;
+
+public static class ServiceCollectionManager
+{
+    public static IServiceProvider ServiceProvider { get; private set; }
+
+    public static void SetServiceProvider(IServiceCollection serviceCollection)
+    {
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+    }
+}
